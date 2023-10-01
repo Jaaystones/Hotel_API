14468a7ec09d0f3cf13c34f94069e2eb9ce2a861
Sammywens <nwene.samuel@gmail.com>
2023-10-01 09:12:54 +0100

    :hammer: Hotel Schema

---
+++ models/rooms.js
@@ -0,0 +1,31 @@
+Import mongoose from "mongoose";
+
+
+const roomSchema = await mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    maxPeople: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    roomNumbers: {
+        type: [{number: Number, unavailableDate: {type: [Date]}}],
+        required: true
+    }
+} 
+{timestamps: true});
+
+const Room = mongoose.model("Room", roomSchema);
+
+export default Room;
